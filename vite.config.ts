a9b008545f3af9edf8f5e42cccee09bf6d53a3dd
JunTeng1113 <junteng.1113@gmail.com>
2024-12-01 13:30:21 +0800

    update: vite config setting

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
-    port: 3001,
+    port: 3001, // 開發伺服器埠號
   },
+  build: {
+    outDir: 'dist', // 指定構建輸出目錄
+    rollupOptions: {
+      input: './src/main.tsx' // 指定入口文件
+    }
+  }
 });
